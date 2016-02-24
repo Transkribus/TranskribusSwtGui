7cc947fbca44e7d8bde28aadec89ec2a1d95f548
Sebi C <sebastian.colutto@uibk.ac.at>
2016-02-24 11:36:40 +0100

    moved package org.dea.swt to eu.transkribus.swt_canvas

---
+++ src/main/java/eu/transkribus/swt_canvas/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * This package is intended to work as a standalone canvas implementation of a canvas in SWT. 
+ * Do not reference any classes from outside this package, s.t. it can be reused in other project using an SWT canvas.
+ */
+package eu.transkribus.swt_canvas;
