b460fcd128f6e470d4a2c6fc24e646aa57bbb587
maulwurf <maulwurf@maulwurf.maulwurf>
2016-06-22 15:48:01 +0200

    sfdafd

--- src/main/java/eu/transkribus/swt_gui/mainwidget/TrpMainWidget.java
+++ src/main/java/eu/transkribus/swt_gui/mainwidget/TrpMainWidget.java
@@ -1288,13 +1288,7 @@ public class TrpMainWidget {
 		// }
 
 		getTreeListener().detach();
-		
 		ui.getStructureTreeViewer().setSelection(sel, true);
-		
-		for (Object element : selData)
-			ui.getStructureTreeWidget().getTreeViewer().refresh(element, true);
-		
-		
 		getTreeListener().attach();
 	}
 
