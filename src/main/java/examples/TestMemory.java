bfb403eaa6ea89680ff284544bb3cdcb82ca1756
maulwurf <maulwurf@maulwurf.maulwurf>
2016-10-14 13:56:25 +0200

    max heap space set in launch4j

---
+++ src/main/java/examples/TestMemory.java
@@ -0,0 +1,27 @@
+package examples;
+public class TestMemory {
+	
+	public static void main(String [] args) {
+		
+		int mb = 1024*1024;
+		
+		//Getting the runtime reference from system
+		Runtime runtime = Runtime.getRuntime();
+		
+		System.out.println("##### Heap utilization statistics [MB] #####");
+		
+		//Print used memory
+		System.out.println("Used Memory:" 
+			+ (runtime.totalMemory() - runtime.freeMemory()) / mb);
+
+		//Print free memory
+		System.out.println("Free Memory:" 
+			+ runtime.freeMemory() / mb);
+		
+		//Print total available memory
+		System.out.println("Total Memory:" + runtime.totalMemory() / mb);
+
+		//Print Maximum available memory
+		System.out.println("Max Memory:" + runtime.maxMemory() / mb);
+	}
+}
