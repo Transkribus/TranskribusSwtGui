3e450fdc9932691cb095e38f52b29be3bfdd713a
Eva Lang <eva.lang@bistum-passau.de>
2018-01-25 15:53:41 +0100

    Bug fix for testing purposes
    please check CommonExportDialog whether storage login needs to be called
    before Dialog setup!

--- src/test/java/eu/transkribus/swt_gui/dialogs/CommonExportDialogTest.java
+++ src/test/java/eu/transkribus/swt_gui/dialogs/CommonExportDialogTest.java
@@ -4,13 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.eclipse.jface.window.ApplicationWindow;
-import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Display;
 
 import eu.transkribus.core.model.beans.TrpPage;
 import eu.transkribus.swt.util.SWTUtil;
+import eu.transkribus.swt_gui.mainwidget.storage.Storage;
 
 public class CommonExportDialogTest {
 	
@@ -25,6 +25,9 @@ public class CommonExportDialogTest {
 				pages.add(null);
 				pages.add(null);
 				pages.add(null);
+				
+				// set up Storage (for whatever reason this is needed) (@author 2018-01-25)
+				Storage s = Storage.getInstance();
 				
 				CommonExportDialog ced = new CommonExportDialog(getShell(), 0, null, "docName", pages);
 				ced.open();
