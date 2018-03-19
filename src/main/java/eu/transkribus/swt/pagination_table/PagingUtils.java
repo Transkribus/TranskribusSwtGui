cbd4d771676021e9fd1d30dcacdb2c445a8a578c
kahlep <philip.kahle@uibk.ac.at>
2018-03-19 13:54:08 +0100

    add PaginationTable that consumes JaxbPaginatedList

--- src/main/java/eu/transkribus/swt/pagination_table/PagingUtils.java
+++ src/main/java/eu/transkribus/swt/pagination_table/PagingUtils.java
@@ -6,6 +6,8 @@ import org.eclipse.nebula.widgets.pagination.PageableController;
 import org.eclipse.nebula.widgets.pagination.collections.PageResult;
 import org.eclipse.swt.SWT;
 
+import eu.transkribus.core.model.beans.rest.JaxbPaginatedList;
+
 public class PagingUtils {
 
 	public static <T> PageResult<T> loadPage(IPageLoadMethods<T> methods, PageableController c) {
@@ -27,5 +29,23 @@ public class PagingUtils {
 				
 		return new PageResult<T>(items, totalSize);
 	}
+	
+	public static <T extends JaxbPaginatedList<R>, R> PageResult<R> loadPage(IPageLoadMethod<T, R> method, PageableController c) {
+		String sortDirection = null;
+		if (c.getSortDirection()==SWT.UP)
+			sortDirection = "desc";
+		else if (c.getSortDirection()==SWT.DOWN)
+			sortDirection = "asc";
+		
+		int pageSize = c.getPageSize();
+		int pageIndex = c.getPageOffset();
+		int fromIndex = pageIndex;
+		int toIndex = pageIndex + pageSize;
+				
+		T items = method.loadPage(fromIndex, toIndex, c.getSortPropertyName(), sortDirection);
+				
+		return new PageResult<R>(items.getList(), items.getTotal());
+	}
+
 	
 }
