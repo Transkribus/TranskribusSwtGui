e577d90044c8b7303cf9b37b1d1e412aa938751c
Florian Krull <Florian Krull@138.232.125.225>
2018-04-23 16:50:02 +0200

    Added Bag of Tokens with Precision, Recall and F-Measure to the Dialog

--- src/main/java/eu/transkribus/swt_gui/dialogs/ErrorRateDialog.java
+++ src/main/java/eu/transkribus/swt_gui/dialogs/ErrorRateDialog.java
@@ -18,8 +18,10 @@ import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Link;
+import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.TableColumn;
@@ -34,22 +36,31 @@ import eu.transkribus.swt.util.DefaultTableColumnViewerSorter;
 import eu.transkribus.swt.util.SWTUtil;
 import eu.transkribus.swt_gui.pagination_tables.PageLockTablePagination;
 
+
 public class ErrorRateDialog extends Dialog{
 
-	public static final String ERR_LABEL_COL = "Metrics";
-	public static final String ERR_WORD_COL = "Word";
-	public static final String ERR_CHAR_COL = "Character";
+	public static final String ERR_EMPTY_COL = " ";
+	public static final String ERR_WORD_COL = "Error Rate";
+	public static final String ERR_CHAR_COL = "Accuracy";
+	public static final String BAG_PREC_COL = "Precision";
+	public static final String BAG_REC_COL = "Recall";
+	public static final String BAG_FMEA_COL = "F-Measure";
 	public static final String ERR_DESC_COL = "Description";
 	
 	MyTableViewer viewer;
 	
+	
+	
 	private TrpErrorRate resultErr;
 	
 	public final ColumnConfig[] ERR_COLS = new ColumnConfig[] {
-			new ColumnConfig(ERR_LABEL_COL,200),
-			new ColumnConfig(ERR_WORD_COL, 220),
+			new ColumnConfig(ERR_EMPTY_COL, 200),
+			new ColumnConfig(ERR_WORD_COL, 100),
 			new ColumnConfig(ERR_CHAR_COL, 100),
-			new ColumnConfig(ERR_DESC_COL, 500),
+			new ColumnConfig(BAG_PREC_COL, 100),
+			new ColumnConfig(BAG_REC_COL, 100),
+			new ColumnConfig(BAG_FMEA_COL, 100),
+			new ColumnConfig(ERR_DESC_COL, 400),
 		};
 
 	public ErrorRateDialog(Shell parentShell, TrpErrorRate resultErr ) {
@@ -76,16 +87,14 @@ public class ErrorRateDialog extends Dialog{
 	    Table table = viewer.getTable();
 		table.setHeaderVisible(true);
 		
-		TableItem itemErr = new TableItem(table, SWT.NONE);
-		itemErr.setText(new String[] { "Error Rate", resultErr.getWer(), resultErr.getCer(),"https://en.wikipedia.org/wiki/Word_error_rate" });
+		TableItem itemWord = new TableItem(table, SWT.NONE);
+		itemWord.setText(new String[] { "Word", resultErr.getWer(), resultErr.getwAcc(),"","","","https://en.wikipedia.org/wiki/Word_error_rate"});
 		
-		TableItem itemAcc = new TableItem(table,SWT.NONE);
-		itemAcc.setText(new String[] { "Accuracy", resultErr.getwAcc(), resultErr.getcAcc(),"https://en.wikipedia.org/wiki/Word_error_rate" });
-		 
-		 
+		TableItem itemChar = new TableItem(table,SWT.NONE);
+		itemChar.setText(new String[] { "Character", resultErr.getCer(), resultErr.getcAcc(),"","","","https://en.wikipedia.org/wiki/Word_error_rate" });
 		
-		
-
+		TableItem itemBag = new TableItem(table,SWT.NONE);
+		itemBag.setText(new String[] {"Bag of Tokens","","", resultErr.getBagTokensPrec(),resultErr.getBagTokensRec(), resultErr.getBagTokensF(), "https://en.wikipedia.org/wiki/F1_score"});
 
 	    return body;
 	  }
