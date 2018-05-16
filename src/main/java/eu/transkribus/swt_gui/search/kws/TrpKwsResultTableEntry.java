63f68523284559283d7fc15312705b0b13ded9e9
Florian Krull <floriankrull1507@yahoo.de>
2018-05-16 16:00:37 +0200

    Changed to fit AJobResultTableEntry

--- src/main/java/eu/transkribus/swt_gui/search/kws/TrpKwsResultTableEntry.java
+++ src/main/java/eu/transkribus/swt_gui/search/kws/TrpKwsResultTableEntry.java
@@ -1,7 +1,5 @@
 package eu.transkribus.swt_gui.search.kws;
 
-import java.text.DecimalFormat;
-import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -16,92 +14,34 @@ import eu.transkribus.core.model.beans.transformer.KwsTransformer;
 import eu.transkribus.core.rest.JobConst;
 import eu.transkribus.core.util.JobDataUtils;
 
-public class TrpKwsResultTableEntry {
-	private static final Logger logger = LoggerFactory.getLogger(TrpKwsResultTableEntry.class);
-	private TrpKwsResult result;
-	private Date created;
-	private String duration;
-	private String query;
-	private String scope;
-	private String status;
-
+public class TrpKwsResultTableEntry extends AJobResultTableEntry<TrpKwsResult> {
 	public TrpKwsResultTableEntry(TrpJobStatus job) {
-//		logger.debug(job.toString());
-		this.created = job.getCreated();
-		if (job.getEndTime() < 1) {
-			this.duration = "N/A";
-		} else {
-			final long diff = job.getEndTime() - job.getStartTime();
-			this.duration = KwsTransformer.DECIMAL_FORMAT.format(diff / 1000f) + " sec.";
-		}
-		this.scope = job.getDocId() < 1 ? "Collection " + job.getColId() : "Document " + job.getDocId();
-		switch(job.getState()) {
-		case TrpJobStatus.RUNNING:
-			this.status = "Processing...";
-			break;
-		case TrpJobStatus.FAILED:
-			this.status = "Failed. See job overview for more info.";
-			break;
-		default:
-			this.status = "Completed";
-			break;
-		}
-		TrpProperties props = job.getJobDataProps();
-		this.result = KwsTransformer.extractResultDataFromProps(props);
+		super(job);
+	}
+
+	private static final Logger logger = LoggerFactory.getLogger(TrpKwsResultTableEntry.class);
+
+	@Override
+	protected TrpKwsResult extractResult(TrpProperties props) {
+		return KwsTransformer.extractResultDataFromProps(props);
+		
+	}
+
+	@Override
+	protected String extractQueries(TrpProperties props, TrpKwsResult result) {
+		String query;
 		if(result == null) {
 			List<String> queries = JobDataUtils.getStringList(props.getProperties(), JobConst.PROP_QUERY);
-			this.query = "\"" + StringUtils.join(queries, "\", \"") + "\"";
+			query = "\"" + StringUtils.join(queries, "\", \"") + "\"";
 		} else {
 			List<String> queriesWithHits = new LinkedList<>();
 			result.getKeyWords()
 				.forEach(k -> queriesWithHits
 							.add("\"" + k.getKeyWord() + "\" (" + k.getHits().size() + ")")
 							);
-			this.query = StringUtils.join(queriesWithHits, ", ");
+			query = StringUtils.join(queriesWithHits, ", ");
 		}
-	}
-
-	public TrpKwsResult getResult() {
-		return result;
-	}
-
-	public Date getCreated() {
-		return created;
-	}
-
-	public void setCreated(Date created) {
-		this.created = created;
-	}
-
-	public String getDuration() {
-		return duration;
-	}
-
-	public void setDuration(String duration) {
-		this.duration = duration;
-	}
-
-	public String getQuery() {
 		return query;
 	}
-
-	public void setQuery(String query) {
-		this.query = query;
-	}
-
-	public String getScope() {
-		return scope;
-	}
-
-	public void setScope(String scope) {
-		this.scope = scope;
-	}
-
-	public String getStatus() {
-		return status;
-	}
-
-	public void setStatus(String status) {
-		this.status = status;
-	}
+	
 }
