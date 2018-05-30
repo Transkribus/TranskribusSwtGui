cb99d944a76d02ccf7df3d62f2a34dcc63b058c8
Florian Krull <floriankrull1507@yahoo.de>
2018-05-30 15:55:15 +0200

    Set entries on ErrorTableViewer

--- src/main/java/eu/transkribus/swt_gui/tool/error/ErrorTableViewer.java
+++ src/main/java/eu/transkribus/swt_gui/tool/error/ErrorTableViewer.java
@@ -1,10 +1,12 @@
 package eu.transkribus.swt_gui.tool.error;
 
-import java.util.List;
-
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableItem;
 
 import eu.transkribus.core.model.beans.TrpErrorList;
+import eu.transkribus.core.model.beans.TrpErrorRate;
 import eu.transkribus.swt.mytableviewer.ColumnConfig;
 import eu.transkribus.swt.mytableviewer.MyTableViewer;
 
@@ -31,12 +33,13 @@ public class ErrorTableViewer extends MyTableViewer {
 			new ColumnConfig(BAG_PREC_COL, 150), new ColumnConfig(BAG_REC_COL, 150),
 			new ColumnConfig(BAG_FMEA_COL, 150), };
 	
-	public void setEntriesList(List<TrpErrorList> pageList) {
-		
-		for (TrpErrorList p : pageList) {
-			
-		}
+	public void setEntriesList(Table table, TrpErrorRate rate) {
+		for (TrpErrorList p : rate.getList()) {
+			TableItem item = new TableItem(table, SWT.NONE);
+			item.setText(new String[] { "Page "+p.getPageNumber(), p.getWer(), p.getCer(),p.getwAcc(),p.getcAcc(),p.getBagTokensPrec(),p.getBagTokensRec(),p.getBagTokensF()});
+		}	
 	}
+	
 
 
 }
