3b65b196bee242edea380760e63b78ad81308711
Florian Krull <floriankrull1507@yahoo.de>
2018-06-08 15:26:10 +0200

    Set default sorting

--- src/main/java/eu/transkribus/swt_gui/tool/error/ErrorTableViewer.java
+++ src/main/java/eu/transkribus/swt_gui/tool/error/ErrorTableViewer.java
@@ -18,14 +18,14 @@ public class ErrorTableViewer extends MyTableViewer {
 	
 	
 	public final ColumnConfig[] ERR_COLS = new ColumnConfig[] { 
-			new ColumnConfig(ERR_PAGE_COL, 100,false, ErrorTableColumnViewerSorter.ASC),
-			new ColumnConfig(ERR_WORD_COL, 150,false,ErrorTableColumnViewerSorter.ASC),
-			new ColumnConfig(ERR_CHAR_COL, 150,false,ErrorTableColumnViewerSorter.ASC),
-			new ColumnConfig(ACC_WORD_COL, 150,false,ErrorTableColumnViewerSorter.ASC ),
-			new ColumnConfig(ACC_CHAR_COL, 150,false,ErrorTableColumnViewerSorter.ASC ),
-			new ColumnConfig(BAG_PREC_COL, 150,false,ErrorTableColumnViewerSorter.ASC ),
-			new ColumnConfig(BAG_REC_COL, 150,false,ErrorTableColumnViewerSorter.ASC ),
-			new ColumnConfig(BAG_FMEA_COL, 150,false,ErrorTableColumnViewerSorter.ASC ) };
+			new ColumnConfig(ERR_PAGE_COL, 100,true),
+			new ColumnConfig(ERR_WORD_COL, 150,true),
+			new ColumnConfig(ERR_CHAR_COL, 150,true),
+			new ColumnConfig(ACC_WORD_COL, 150,true ),
+			new ColumnConfig(ACC_CHAR_COL, 150,true ),
+			new ColumnConfig(BAG_PREC_COL, 150,true ),
+			new ColumnConfig(BAG_REC_COL, 150,true ),
+			new ColumnConfig(BAG_FMEA_COL, 150,true ) };
 	
 	public ErrorTableViewer(Composite parent, int style) {
 		super(parent, style);
