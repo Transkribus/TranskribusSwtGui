9aa959786c8ffe7e2e192d2d3609fad5de2fc606
hackmanschorsch <guenter.hackl@uibk.ac.at>
2018-10-15 11:27:36 +0200

    add several pages instead of just one (changed method call in main menu)

--- src/main/java/eu/transkribus/swt_gui/mainwidget/menubar/TrpMenuBar.java
+++ src/main/java/eu/transkribus/swt_gui/mainwidget/menubar/TrpMenuBar.java
@@ -138,7 +138,7 @@ public class TrpMenuBar {
 		replaceImageItem = createItem(fileMenu.m, SWT.NONE, Images.IMAGE_EDIT, "Replace image of current page on server...");
 		openLocalPageFileItem = createItem(fileMenu.m, 0, null, "Open local page file for current page...");		
 		deletePageMenuItem = createItem(fileMenu.m, 0, null, "Delete current page from server");
-		addPageMenuItem = createItem(fileMenu.m, 0, null, "Add page to document on server...");
+		addPageMenuItem = createItem(fileMenu.m, 0, null, "Add page(s) to document on server...");
 		syncWordsWithLinesMenuItem = createItem(fileMenu.m, SWT.NONE, null, "Sync word transcription with text in lines");
 		
 		collMenu = createCascadeMenu(menuBar, null, "&Collections");
