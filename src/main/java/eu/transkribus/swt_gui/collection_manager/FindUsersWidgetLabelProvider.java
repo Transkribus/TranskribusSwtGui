88d240a47283aa1d12ce995b77cfc608df51c10f
kahlep <philip.kahle@uibk.ac.at>
2019-02-04 11:24:34 +0100

    Transkribus/TranskribusServer#51: FindUsersWidget shows inactive state

--- src/main/java/eu/transkribus/swt_gui/collection_manager/FindUsersWidgetLabelProvider.java
+++ src/main/java/eu/transkribus/swt_gui/collection_manager/FindUsersWidgetLabelProvider.java
@@ -4,20 +4,25 @@ import org.eclipse.jface.viewers.ILabelProviderListener;
 import org.eclipse.jface.viewers.ITableColorProvider;
 import org.eclipse.jface.viewers.ITableLabelProvider;
 import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.TableColumn;
 
 import eu.transkribus.core.model.beans.auth.TrpUser;
+import eu.transkribus.swt.util.Colors;
 
 public class FindUsersWidgetLabelProvider implements ITableLabelProvider, ITableColorProvider {
 	TableViewer tv;
 	Table table;
 	
+	Color greyedOutTextColor;
+	
 	public FindUsersWidgetLabelProvider(TableViewer tv) {
 		this.tv = tv;
 		this.table = tv.getTable();
+		this.greyedOutTextColor = Colors.getSystemColor(SWT.COLOR_GRAY);
 	}
 	
 	@Override public String getColumnText(Object element, int columnIndex) {
@@ -28,7 +33,11 @@ public class FindUsersWidgetLabelProvider implements ITableLabelProvider, ITable
 			String ct = column.getText();
 					
 			if (ct.equals(FindUsersWidget.USER_USERNAME_COL)) {
-				return user.getUserName();
+				if(user.getIsActive() > 0) {
+					return user.getUserName();
+				} else {
+					return user.getUserName() + " (activation pending)";
+				}
 			} else if (ct.equals(FindUsersWidget.USER_FULLNAME_COL)) {
 				return user.getFirstname()+" "+user.getLastname();
 			}
@@ -56,6 +65,12 @@ public class FindUsersWidgetLabelProvider implements ITableLabelProvider, ITable
 	}
 
 	@Override public Color getForeground(Object element, int columnIndex) {
+		if (element instanceof TrpUser) {
+			TrpUser user = (TrpUser) element;					
+			if (user.getIsActive() < 1) {
+				return greyedOutTextColor;
+			}
+		}
 		return null;
 	}
 
