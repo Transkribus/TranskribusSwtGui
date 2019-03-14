29d1f7951776f611ca17e3f4350351b1b2524b37
kahlep <philip.kahle@uibk.ac.at>
2019-03-14 12:22:45 +0100

    HtrGroundTruthLabelAndFontProvider marks loaded GT sets with bold font

---
+++ src/main/java/eu/transkribus/swt_gui/htr/treeviewer/HtrGroundTruthLabelAndFontProvider.java
@@ -0,0 +1,46 @@
+package eu.transkribus.swt_gui.htr.treeviewer;
+
+import org.eclipse.jface.viewers.IFontProvider;
+import org.eclipse.swt.graphics.Font;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import eu.transkribus.swt.util.Fonts;
+import eu.transkribus.swt_gui.htr.treeviewer.HtrGroundTruthContentProvider.HtrGtDataSet;
+import eu.transkribus.swt_gui.htr.treeviewer.HtrGroundTruthContentProvider.HtrGtDataSetElement;
+import eu.transkribus.swt_gui.htr.treeviewer.HtrGroundTruthContentProvider.TrpHtrGtDocMetadata;
+import eu.transkribus.swt_gui.mainwidget.storage.Storage;
+
+/**
+ * HtrGroundTruthLabelAndFontProvider that sets a bold font on the GT pages loaded in the MainWidget/Storage 
+ */
+public class HtrGroundTruthLabelAndFontProvider extends HtrGroundTruthLabelProvider implements IFontProvider {
+	private static final Logger logger = LoggerFactory.getLogger(HtrGroundTruthLabelAndFontProvider.class);
+	
+	protected final Font boldFont;
+	protected Storage store;
+	public HtrGroundTruthLabelAndFontProvider(Font defaultFont) {
+		super();
+		this.store = Storage.getInstance();
+		this.boldFont = Fonts.createBoldFont(defaultFont);
+	}
+	@Override
+	public Font getFont(Object element) {
+		if(!store.isGtDoc()) {
+			return null;
+		}
+		HtrGtDataSet loadedSet = ((TrpHtrGtDocMetadata) store.getDoc().getMd()).getDataSet();
+
+		if(element instanceof HtrGtDataSet) {
+			logger.debug("getFont for " + element + " | loadedSet = " + loadedSet);
+			if(((HtrGtDataSet)element).equals(loadedSet)) {
+				return boldFont;
+			}
+		} else if (element instanceof HtrGtDataSetElement 
+				&& ((HtrGtDataSetElement)element).getParentHtrGtDataSet().equals(loadedSet) 
+				&& ((HtrGtDataSetElement)element).getGroundTruthPage().getPageNr() == store.getPage().getPageNr()) {
+			return boldFont;
+		}
+		return null;
+	}
+}
