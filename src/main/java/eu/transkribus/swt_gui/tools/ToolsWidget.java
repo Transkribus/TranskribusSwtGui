becb13f741928a72b31fd20064aabc2a940a03cf
jkloe <jkloe@jkloe.com>
2019-04-10 09:20:46 +0200

    Modify label for P2PaLA tool button

--- src/main/java/eu/transkribus/swt_gui/tools/ToolsWidget.java
+++ src/main/java/eu/transkribus/swt_gui/tools/ToolsWidget.java
@@ -439,8 +439,8 @@ public class ToolsWidget extends Composite {
 		p2palaContainer.setLayout(SWTUtil.createGridLayout(3, false, 0, 0));
 		
 		p2palaBtn = new Button(p2palaContainer, SWT.PUSH);
-		p2palaBtn.setText("Start P2PaLA layout analysis");
-		p2palaBtn.setToolTipText("P2PaLA layout analysis - creates baselines, regions and structure tags");
+		p2palaBtn.setText("P2PaLA layout analysis");
+		p2palaBtn.setToolTipText("P2PaLA layout analysis - creates baselines, regions and structure tags depending on the models selected on the right");
 		p2palaBtn.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
 		
 		p2palaModelCombo = new Combo(p2palaContainer, SWT.READ_ONLY | SWT.DROP_DOWN);
