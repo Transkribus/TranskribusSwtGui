66264a7f2ce7ad5f17de8f3ba7f488909e8cd0e7
kahlep <philip.kahle@uibk.ac.at>
2019-09-26 16:15:18 +0200

    Add note on dataset privacy in GroundTruthTreeWidget

--- src/main/java/eu/transkribus/swt_gui/htr/treeviewer/HtrGroundTruthTableLabelAndFontProvider.java
+++ src/main/java/eu/transkribus/swt_gui/htr/treeviewer/HtrGroundTruthTableLabelAndFontProvider.java
@@ -60,7 +60,12 @@ public class HtrGroundTruthTableLabelAndFontProvider extends CellLabelProvider i
 			if(element instanceof TrpHtr) {
 				return ((TrpHtr)element).getName();
 			} else if (element instanceof HtrGtDataSet) {
-				return ((HtrGtDataSet)element).getDataSetType().getLabel() ;
+				HtrGtDataSet set = (HtrGtDataSet) element;
+				String suffix = "";
+				if(!store.isUserAllowedToViewDataSets(set.getHtr())) {
+					suffix = " (private)";
+				}
+				return set.getDataSetType().getLabel() + suffix;
 			} else if (element instanceof HtrGtDataSetElement) {
 				return "Page " + ((HtrGtDataSetElement)element).getGroundTruthPage().getPageNr();
 			}
