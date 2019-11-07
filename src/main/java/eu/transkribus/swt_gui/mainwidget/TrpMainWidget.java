6991820fe2707e7c52b27f15995d4c2327e1305c
krf41037 <floriankrull1507@yahoo.de>
2019-11-07 10:34:22 +0100

    Set iiifValidator API for clearer error message

--- src/main/java/eu/transkribus/swt_gui/mainwidget/TrpMainWidget.java
+++ src/main/java/eu/transkribus/swt_gui/mainwidget/TrpMainWidget.java
@@ -3218,7 +3218,7 @@ public class TrpMainWidget {
 					URL url = new URL(ud.getIiifUrl());
 					IIIFUtils.checkManifestValid(url);
 					valid = true;
-				} catch(JsonMappingException | JsonParseException e) {
+				} catch(IllegalArgumentException|JsonMappingException | JsonParseException e) {
 					DialogUtil.showDetailedErrorMessageBox(getShell(), "Manifest not valid",
 							"Upload document from IIIF manifest not possible!\n"
 							+ "Note: IIIF manifest is not valid and therefore cannot be parsed.", e.getMessage());
