bfd044a12371227a55b95883886d6924a672adef
kahlep <philip.kahle@uibk.ac.at>
2019-12-04 15:48:33 +0100

    GroundTruthWidget allows duplicating GT sets to documents via context menu

--- src/main/java/eu/transkribus/swt_gui/htr/treeviewer/GroundTruthTreeWidget.java
+++ src/main/java/eu/transkribus/swt_gui/htr/treeviewer/GroundTruthTreeWidget.java
@@ -2,23 +2,47 @@ package eu.transkribus.swt_gui.htr.treeviewer;
 
 import java.util.List;
 
+import javax.ws.rs.ClientErrorException;
+import javax.ws.rs.ServerErrorException;
+
+import org.apache.commons.lang3.StringUtils;
 import org.eclipse.jface.viewers.AbstractTreeViewer;
 import org.eclipse.jface.viewers.CellLabelProvider;
 import org.eclipse.jface.viewers.ITreeContentProvider;
 import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.jface.viewers.TreeViewerColumn;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.MenuDetectEvent;
+import org.eclipse.swt.events.MenuDetectListener;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.widgets.MenuItem;
+import org.eclipse.swt.widgets.TreeItem;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import eu.transkribus.client.util.SessionExpiredException;
+import eu.transkribus.core.model.beans.GroundTruthSelectionDescriptor;
+import eu.transkribus.core.model.beans.TrpCollection;
 import eu.transkribus.core.model.beans.TrpHtr;
+import eu.transkribus.core.model.beans.TrpHtr.ReleaseLevel;
+import eu.transkribus.swt.util.DialogUtil;
 import eu.transkribus.swt.util.Images;
+import eu.transkribus.swt_gui.dialogs.ChooseCollectionDialog;
 import eu.transkribus.swt_gui.htr.HtrFilterWidget;
+import eu.transkribus.swt_gui.htr.treeviewer.HtrGroundTruthContentProvider.HtrGtDataSet;
+import eu.transkribus.swt_gui.mainwidget.TrpMainWidget;
+import eu.transkribus.swt_gui.mainwidget.storage.Storage;
+import eu.transkribus.swt_gui.mainwidget.storage.StorageUtil;
 import eu.transkribus.swt_gui.structure_tree.StructureTreeWidget.ColConfig;
 
 public class GroundTruthTreeWidget extends Composite {
+	private static final Logger logger = LoggerFactory.getLogger(GroundTruthTreeWidget.class);
 
 	private TreeViewer treeViewer;
 	private final CellLabelProvider labelProvider;
@@ -27,6 +51,8 @@ public class GroundTruthTreeWidget extends Composite {
 	
 	private Composite filterWidget;
 	private Button reloadBtn;
+	
+	MenuItem copyGtSetToDocItem;
 	
 	//TODO paging
 	//ToolItem clearPageItem, deleteSelectedBtn;
@@ -74,6 +100,12 @@ public class GroundTruthTreeWidget extends Composite {
 		reloadBtn.setImage(Images.getOrLoad("/icons/refresh.gif"));
 		reloadBtn.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, true));
 
+		Menu menu = new Menu(treeViewer.getTree());
+		treeViewer.getTree().setMenu(menu);
+
+		copyGtSetToDocItem = new MenuItem(menu, SWT.NONE);
+		copyGtSetToDocItem.setText("Copy data set to new document...");
+		
 		initCols();
 		
 		initListener();
@@ -100,7 +132,86 @@ public class GroundTruthTreeWidget extends Composite {
 	}
 	
 	void initListener() {
-		//TODO add listeners for paging tool items
+		treeViewer.getTree().addMenuDetectListener(new MenuDetectListener() {
+			
+			@Override
+			public void menuDetected(MenuDetectEvent event) {
+				if (treeViewer.getTree().getSelectionCount() != 1) {
+					event.doit = false;
+					return;
+				}
+				TreeItem selection = treeViewer.getTree().getSelection()[0];
+				Object selectionData = selection.getData();
+				
+				logger.debug("Menu detected on tree item of type: {}", selectionData.getClass());
+				
+				if(!(selectionData instanceof HtrGtDataSet)) {
+					event.doit = false;
+					return;
+				}
+				HtrGtDataSet gtSet = (HtrGtDataSet) selectionData;
+				TrpHtr htr = gtSet.getModel();
+				boolean isDataSetAccessible = htr.getCollectionIdLink() != null 
+						|| htr.getReleaseLevelValue() >= ReleaseLevel.DisclosedDataSet.getValue();
+				
+				if(!Storage.getInstance().isAdminLoggedIn() && !isDataSetAccessible) {
+					logger.debug("Data set not accessible for this user.");
+					event.doit = false;
+					return;
+				}
+				
+				if (!StorageUtil.canDuplicate(Storage.getInstance().getCollId())) {
+					logger.debug("User not privileged to manage collection.");
+					event.doit = false;
+					return;
+				}
+				
+				//show the menu
+			}
+		});
+		
+		copyGtSetToDocItem.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				ChooseCollectionDialog ccd = new ChooseCollectionDialog(getShell());
+				
+				@SuppressWarnings("unused")
+				int ret = ccd.open();
+				TrpCollection col = ccd.getSelectedCollection();
+				
+				if(col == null) {
+					logger.debug("No collection was selected.");
+					return;
+				}
+				
+				//MenuDetectListener determined that this action is fine for the selection. Only HtrGtDataSet is allowed now
+				TreeItem selection = treeViewer.getTree().getSelection()[0];
+				Object selectionData = selection.getData();
+				
+				if(selectionData == null) {
+					logger.debug("Menu aborted without selection.");
+				}
+				
+				HtrGtDataSet gtSet = (HtrGtDataSet) selectionData;
+
+				final String title = "Copy of HTR " + gtSet.getDataSetType().getLabel() + "'" + gtSet.getModel().getName() + "'";
+				
+				GroundTruthSelectionDescriptor desc = new GroundTruthSelectionDescriptor(gtSet.getId(), gtSet.getDataSetType().toString());
+				
+				try {
+					TrpMainWidget.getInstance().duplicateGtToDocument(col, desc, title);
+				} catch (SessionExpiredException | ServerErrorException | ClientErrorException e1) {
+					logger.debug("Could copy dataset to collection!", e1);
+					String errorMsg = "The data set could not be copied to this collection.";
+					if(!StringUtils.isEmpty(e1.getMessage())) {
+						errorMsg += "\n" + e1.getMessage();
+					}
+					DialogUtil.showErrorMessageBox(getShell(), "Error while copying data set",
+							errorMsg);
+				}
+				super.widgetSelected(e);
+			}
+		});
 	}
 	
 	private void initCols() {
