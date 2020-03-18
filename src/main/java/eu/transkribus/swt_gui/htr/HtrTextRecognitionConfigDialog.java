6db4d07548340997df6170e99065138545c5a27b
jkloe <jkloe@jkloe.com>
2020-03-18 17:21:57 +0100

    activate integrated LM selection for PyLaia decoding

--- src/main/java/eu/transkribus/swt_gui/htr/HtrTextRecognitionConfigDialog.java
+++ src/main/java/eu/transkribus/swt_gui/htr/HtrTextRecognitionConfigDialog.java
@@ -70,7 +70,7 @@ public class HtrTextRecognitionConfigDialog extends Dialog {
 		
 		Group dictGrp = new Group(sash, SWT.NONE);
 		dictGrp.setLayout(new GridLayout(1, false));
-		dictGrp.setText("Dictionary");
+		dictGrp.setText("Language Model");
 		
 		htrDictComp = new HtrDictionaryComposite(dictGrp, 0);
 		htrDictComp.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
@@ -98,11 +98,14 @@ public class HtrTextRecognitionConfigDialog extends Dialog {
 		final String provider = htrModelsComp.getSelectedHtr().getProvider();
 		if (!HtrPyLaiaUtils.doesDecodingSupportDicts() 
 				&& provider.equals(HtrPyLaiaUtils.PROVIDER_PYLAIA)) {
-			sash.setWeights(new int[] { 100, 0 });
+//			sash.setWeights(new int[] { 100, 0 });
+//			htrDictComp.updateUi(false, htrModelsComp.getSelectedHtr().isLanguageModelExists(), false);
+			htrDictComp.updateUi(false, true, false);
+			sash.setWeights(new int[] { 88, 12 });			
 		} else if (provider.equals(HtrCITlabUtils.PROVIDER_CITLAB_PLUS)
 				|| provider.equals(HtrCITlabUtils.PROVIDER_CITLAB)) {
 			//show option to select integrated dictionary if available for this model
-			htrDictComp.updateUi(false, htrModelsComp.getSelectedHtr().isLanguageModelExists());
+			htrDictComp.updateUi(false, htrModelsComp.getSelectedHtr().isLanguageModelExists(), true);
 			sash.setWeights(new int[] { 88, 12 });
 		} else {
 			sash.setWeights(new int[] { 88, 12 });
@@ -121,7 +124,7 @@ public class HtrTextRecognitionConfigDialog extends Dialog {
 			
 			TrpHtr selHtr = htrModelsComp.getSelectedHtr();
 			boolean showLangModOption = selHtr != null && selHtr.isLanguageModelExists();
-			htrDictComp.updateUi(false, showLangModOption);
+			htrDictComp.updateUi(false, showLangModOption, true);
 			
 			htrDictComp.updateSelection(config.getDictionary());
 			break;
